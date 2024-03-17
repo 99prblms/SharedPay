b18bac25dbf9a3dbf75dc9c659b6246f55476430
99prblms <101308224+99prblms@users.noreply.github.com>
2024-03-17 16:56:53 +0500

    Передача данных MVC

--- SharedPay/CheckScreen/CheckModel.swift
+++ SharedPay/CheckScreen/CheckModel.swift
@@ -9,4 +9,13 @@ import Foundation
 
 class CheckModel {
     
-}
+        func chitaemSummX2(summ: String?, pers: String?, tips: String?) -> StructSumm? {
+            guard let summStr = summ, let persStr = pers, let tipsStr = tips,
+                  let summInt = Int(summStr), let persInt = Int(persStr), let tipsInt = Int(tipsStr)
+            else {
+                print("Ошибка: невозможно преобразовать значения в числа.")
+                return nil
+            }
+            return StructSumm(yourCheck: summInt * 2, yourTips: tipsInt * 2, yourWP: ((summInt / persInt) + (tipsInt / persInt)) * 2)
+        }
+    }
--- SharedPay/CheckScreen/CheckViewController.swift
+++ SharedPay/CheckScreen/CheckViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class CheckViewController: UIViewController {
     
-    lazy var mainView = CheckView()
-//    var payVC = PaymentViewController()
+    var dataQW: StructSumm? {
+        didSet {
+            if let data = dataQW {
+                mainView.updateView(with: data)
+            }
+        }
+    }
+    
+    var dataQWX2: StructSumm? {
+        didSet {
+            if let data = dataQWX2 {
+                mainView.updateViewX2(with: data)
+            }
+        }
+    }
+    
+    var mainModel = CheckModel()
+    lazy var mainView = CheckView(delegate: self)
     
     override func loadView() {
         view = mainView
@@ -18,7 +34,17 @@ class CheckViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        func updateViewWithData() {
+        }
     }
+}
+
+extension CheckViewController: CheckViewDelegate {
+    func dontPressX2(SummCheckX2: String, YCheckX2: String, YTipsX2: String) {
+        dataQWX2 = mainModel.chitaemSummX2(summ: SummCheckX2, pers: YCheckX2, tips: YTipsX2)
+    }
+    
+    
 }
 
